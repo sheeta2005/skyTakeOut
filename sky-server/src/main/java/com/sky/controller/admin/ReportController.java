2a4b657026de76223824f9f761d5960afe27df32
sheeta1998 <2169298883@qq.com>
2026-05-07 21:08:31 +0800

    Day12数据导表

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -76,4 +77,13 @@ public class ReportController {
         log.info("销量排名top10：{},{}", begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
     }
+
+
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据库报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
+    }
+
+
 }
